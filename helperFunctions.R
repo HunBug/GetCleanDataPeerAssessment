4907bba8f7bb99d4cc7a5ab42a8df63d88ccce86
HunBug <akos.strack+github@gmail.com>
2014-04-24 21:09:43 +0200

    working version

--- helperFunctions.R
+++ helperFunctions.R
@@ -1,26 +1,38 @@
 loadUciData <- function(directory,setName)
 {
-  activityLabelsFilename <- paste(getwd(),"/",directory,"/activity_labels.txt", sep="") 
   featureLabelsFilename <- paste(getwd(),"/",directory,"/features.txt", sep="") 
   featuresFilename <- paste(getwd(),"/",directory,"/",setName,"/X_",setName, ".txt", sep="")
   outputFilename <- paste(getwd(),"/",directory,"/",setName,"/y_",setName, ".txt", sep="")
   subjectsFilename <- paste(getwd(),"/",directory,"/",setName,"/subject_",setName, ".txt", sep="")
   
   featureLabels <- read.table(featureLabelsFilename,header=FALSE)
-  activityLabels <- read.table(activityLabelsFilename,header=FALSE)
   features <- read.table(featuresFilename,header=FALSE)
   output <- read.table(outputFilename,header=FALSE)
   subjects <- read.table(subjectsFilename,header=FALSE)
   
   colnames(features) <- featureLabels[,2]
   
-  outputLabel <- factor(output[,1],levels=activityLabels[,1],labels=activityLabels[,2])
   features <- cbind.data.frame(features,output)
   colnames(features)[ncol(features)] <- "actNum"
+  outputLabel <- addLabels(directory,features[,ncol(features)])
   features <- cbind.data.frame(features,outputLabel)
   colnames(features)[ncol(features)] <- "actLab"
   features <- cbind.data.frame(features,subjects)
   colnames(features)[ncol(features)] <- "subject"
   
   return(features)
+}
+
+getActivityLabels <- function(directory)
+{
+  activityLabelsFilename <- paste(getwd(),"/",directory,"/activity_labels.txt", sep="") 
+  activityLabels <- read.table(activityLabelsFilename,header=FALSE)
+  return(activityLabels)
+}
+
+addLabels <- function(directory,outputNum)
+{
+  activityLabels <- getActivityLabels(directory)
+  outputLabel <- factor(outputNum,levels=activityLabels[,1],labels=activityLabels[,2]) 
+  return(outputLabel)
 }
--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,16 @@
+dataDirectory = "UCI HAR Dataset"
+print("Loading helper functions")
+source("helperFunctions.R")
 print("Loading test...")
-testData <- loadUciData("UCI HAR Dataset","test")
+testData <- loadUciData(dataDirectory,"test")
 print("Loading train...")
-trainData <- loadUciData("UCI HAR Dataset","train")
+trainData <- loadUciData(dataDirectory,"train")
 print("Merging...")
 data <- rbind.data.frame(trainData,testData)
 print("Subsetting to first dataset...")
 firstSet <- data[,grep("mean|std|actLab",colnames(data))]
 print("Aggregating second dataset...")
 secondSet <- aggregate(data[,-563],by=list(data$actNum,data$actLab,data$subject), FUN=mean)
+outputLabel <- addLabels(dataDirectory,secondSet[,ncol(secondSet)-1])
+secondSet <- cbind.data.frame(secondSet,outputLabel)
+colnames(secondSet)[ncol(secondSet)-1] <- "actLab"
